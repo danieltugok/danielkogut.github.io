fec038b4e028ea7a323827953b62737857d1612b
Daniel Kogut <danieltugok@gmail.com>
2022-06-20 17:36:24 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:danietugok/danieltugok.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:danietugok/danietugok.git main:gh-pages
+git push -f git@github.com:danietugok/danietugok.git gh-pages
 
 cd -
